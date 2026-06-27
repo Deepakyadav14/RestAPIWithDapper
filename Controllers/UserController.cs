0c3f066c58499263a3aabed894a2a40d70e806d5
DeepakYadav14 <deepyad14@gmail.com>
2026-06-27 23:40:45 +0530

    first commit of api

---
+++ Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Dapper;
+
+namespace RestFul_APIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IDbConnectionFactory _dbFactory;
+        public UserController(IDbConnectionFactory dbFactory)
+        {
+            _dbFactory = dbFactory;
+        }
+
+        [HttpGet("/GetProjects")]
+       public async Task<IActionResult> GetPorjectList()
+       {
+            using var connection = _dbFactory.CreateConnection();
+            string sql = "SELECT TOP (100) [Id],[Title],[Subtitle],[Description],[Sector],[City],[FeaturesJson] FROM [AyansVillas].[dbo].[Projects]";
+            var data =await connection.QueryAsync<ProjectsDetails>(sql);
+            return Ok(data);
+                
+       }
+    }
+    
+    public class ProjectsDetails
+    {
+       public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? SubTitle { get; set; }
+        public string?  Description { get; set; }
+        public string? Sector { get; set; }
+        public string? City { get; set; }
+        public string[]? FeaturedJson { get; set; }
+
+    }
+
+}
---
+++ DbConnectionFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Connections;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace RestFul_APIs
+{
+    public interface IDbConnectionFactory
+    {
+        IDbConnection CreateConnection();
+    }
+    public class DbConnectionFactory : IDbConnectionFactory
+    {
+        private readonly string _connectionString;
+
+        public DbConnectionFactory(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConn");
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
